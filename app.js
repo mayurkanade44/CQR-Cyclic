2441c3d26f14e3cd2ac19db594726b33e3b11d63
mayurkanade44 <mayurkanade44@gmail.com>
2023-01-05 16:33:31 +0530

    cors added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("express-async-errors");
+const cors = require("cors");
 
 const express = require("express");
 const app = express();
@@ -37,6 +38,7 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.use(express.static("./public"));
+app.use(cors());
 app.use(express.json());
 app.use(uploadImage({ useTempFiles: true }));
 
